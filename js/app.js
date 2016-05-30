89744813fcad46b0cd68dffaf8e8013c945026c3
Paul Davis <paulandcindy@gmail.com>
2016-05-29 22:09:59 -0400

    feat: Add Openweatermap api
    Add call to OpenweatherMap API for weather info. Add Google map with
    current location.
    Add page styling.

--- js/app.js
+++ js/app.js
@@ -8,82 +8,91 @@ var main = function(){
   var state;
   var formattedCityState;
   var domLocation = document.getElementById("cityLocation");
+  var units;
 
-
-  // put ajax call to open weather here 
-
-  // api.openweathermap.org/data/2.5/forecast?lat=35&lon=139 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  var getWeather = function(){
+    units = "&units=imperial";
+    var url = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+units+"&APPID=9ad795d5b78d90a7040ffb074441a24f";
+    $.ajax({
+      type: 'GET',
+      url: url,
+      contentType: "application/json",
+      dataType: 'jsonp',
+      success: function(data) {
+        $('.pageHeader').text('Local Weather');
+        $('.conditions').html('Currently in ' + city +': ' + data.weather[0].main);
+        $('.temp').html('Current ' + Math.floor(Math.round(data.main.temp)) + '<p> Today\'s High ' + Math.floor(Math.round(data.main.temp_max)) + ' </p>' + '<p> Today\'s Low ' + Math.floor(Math.round(data.main.temp_min)) + ' </p>');
+        return;
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log('An error occurred... ');
+        console.log('jqXHR:', jqXHR);
+        console.log('textStatus:', textStatus);
+        console.log('errorThrown:', errorThrown);
+      }
+    }
+  );
+};
 // converts the lat and long to a location
 // places a marker and infowindow label on the map
-  var geocodeLatLng= function(geocoder, map, infowindow) {
-    geocoder.geocode({'location': latlng}, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        if (results[1]) {
-          map.setZoom(11);
-          var marker = new google.maps.Marker({
-            position: latlng,
-            map: map
-          });
-          cityState = results[1].formatted_address;
-          city = results[1].address_components[1].long_name;
-          state = results[1].address_components[3].short_name;
-          formattedCityState = city + ", " + state;
-          infowindow.setContent(formattedCityState);
-          infowindow.open(map, marker);
-        } else {
-          window.alert('No results found');
-        }
+var geocodeLatLng= function(geocoder, map, infowindow) {
+  geocoder.geocode({'location': latlng}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+        map.setZoom(11);
+        var marker = new google.maps.Marker({
+          position: latlng,
+          map: map
+        });
+        cityState = results[1].formatted_address;
+        city = results[1].address_components[1].long_name;
+        state = results[1].address_components[3].short_name;
+        formattedCityState = city + ", " + state;
+        infowindow.setContent(formattedCityState);
+        infowindow.open(map, marker);
+
       } else {
-        window.alert('Geocoder failed due to: ' + status);
+        window.alert('No results found');
       }
-    });
-  }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
+    formattedCityState = city + ", " + state;
+    getWeather();
+  });
+
+};
 // initialize the google map
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: latlng
-    });
-    var geocoder = new google.maps.Geocoder;
-    var infowindow = new google.maps.InfoWindow;
-    
-    // convert the coordinates in to a location on the map 
-    geocodeLatLng(geocoder, map, infowindow);
-  };
+var initMap = function() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: latlng
+  });
+  var geocoder = new google.maps.Geocoder();
+  var infowindow = new google.maps.InfoWindow();
+
+  // convert the coordinates in to a location on the map
+  geocodeLatLng(geocoder, map, infowindow);
+};
 // function to obtain geolocation from the browser
 // once it completes, it calls the initMap function
-  var getLocation = function(){
-    navigator.geolocation.getCurrentPosition(success, error);
-    function success(position){
-      latitude  = position.coords.latitude;
-      longitude = position.coords.longitude;
-      domLocation.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-      latlng = {lat: latitude, lng: longitude};
+var getLocation = function(){
+  navigator.geolocation.getCurrentPosition(success, error);
+  function success(position){
+    latitude  = position.coords.latitude;
+    longitude = position.coords.longitude;
+    // domLocation.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+    latlng = {lat: latitude, lng: longitude};
 
-      initMap();
-    }
-    // if geolocation fails
-    function error(){
-      console.log('can not find you');
-    }
-  };
+    initMap();
+  }
+  // if geolocation fails
+  function error(){
+    console.log('can not find you');
+  }
+};
 // get the geolocation from the browser
-  getLocation();
+getLocation();
 };
 // called after google maps call returns
 var init = function(){
